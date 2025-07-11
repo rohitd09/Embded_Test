ce2a7245660034a7d06bde9fff2cba2a2ffbc179
GAMING-DISCIPLE\rohit <rohitd3476@gmail.com>
2025-07-11 14:34:37 -0500

    Added Sparse Embeddings to test

--- embed_test/embed_text.py
+++ embed_test/embed_text.py
@@ -1,5 +1,5 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from fastembed import TextEmbedding
+from fastembed import TextEmbedding, SparseTextEmbedding
 
 with open("example.txt", "r", encoding="utf-8") as f: # Read example privacy policy 
     document = f.read()
@@ -10,10 +10,19 @@ texts = text_splitter.split_text(document) # Split the document into recursive c
 model = TextEmbedding(model_name="BAAI/bge-small-en-v1.5") # Dense embedding for semantic search
 embeddings = list(model.embed(texts)) # Pass the list of text to the embedding model
 
-# with open("embeddings.txt", "w", encoding="utf-8") as f:
-#     for text, embedding in zip(texts, embeddings):
-#         f.write(f"Text:\n{text}\n")
-#         f.write("Embedding:\n")
-#         f.write(",".join(f"{x:.6f}" for x in embedding) + "\n")
-#         f.write("\n" + "-" * 80 + "\n\n")
+with open("embed_test/embeddings.txt", "w", encoding="utf-8") as f:
+    for text, embedding in zip(texts, embeddings):
+        f.write(f"Text:\n{text}\n")
+        f.write("Embedding:\n")
+        f.write(",".join(f"{x:.6f}" for x in embedding) + "\n")
+        f.write("\n" + "-" * 80 + "\n\n")
 
+sparse_model = SparseTextEmbedding(model_name="prithivida/Splade_PP_en_v1")
+sparse_embeddings = list(sparse_model.embed(texts))
+
+with open("embed_test/sparse_embeddings.txt", "w", encoding="utf-8") as f:
+    for text, embedding in zip(texts, sparse_embeddings):
+        f.write(f"Text:\n{text}\n")
+        f.write("Embedding (sparse vector as index:weight):\n")
+        f.write(", ".join(f"{idx}:{val:.6f}" for idx, val in zip(embedding.indices, embedding.values)) + "\n")
+        f.write("\n" + "-" * 80 + "\n\n")
--- qdrant_test.py
+++ qdrant_test.py
@@ -1,4 +1,15 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
+from fastembed import TextEmbedding
 
-client = QdrantClient(path=r'C:\Users\rohit\OneDrive\Projects\FoxLint\similarity_search')
+with open("example.txt", "r", encoding="utf-8") as f: # Read example privacy policy 
+    document = f.read()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100) # Approx 256 token size
+texts = text_splitter.split_text(document) # Split the document into recursive chunks
+
+model = TextEmbedding(model_name="BAAI/bge-small-en-v1.5") # Dense embedding for semantic search
+embeddings = list(model.embed(texts)) # Pass the list of text to the embedding model
+
+client = QdrantClient(path=r'C:\Users\rohit\OneDrive\Projects\FoxLint\similarity_search') # Connect to Qdrant Client on local machine
 
